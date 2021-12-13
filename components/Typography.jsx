a44e752f02ebe5d4f267044451ded367b86129c4
ardalan nahavandi <ardalan.nhd@gmail.com>
2021-12-13 19:47:13 +0330

    added a custom typography component that supports global theme object from react native elements

--- components/Typography.jsx
+++ components/Typography.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { makeStyles } from 'react-native-elements'
+import { makeStyles, useTheme } from 'react-native-elements'
 
 export default function Typography({ variant = "p", textStyle, viewStyle, children }) {
+    console.log(useTheme().theme.typography)
     const styles = useStyles(variant)
     return (
         <View style={viewStyle}>
@@ -16,10 +17,10 @@ export default function Typography({ variant = "p", textStyle, viewStyle, childr
 const useStyles = makeStyles((theme, variant) => ({
     text: {
         color: theme.colors.typography,
-        fontFamily: theme.typography[variant].fontFamily ?? theme.typography.default.fontFamily,
-        fontSize: theme.typography[variant].fontSize ?? theme.typography.default.fontSize,
-        fontStyle: theme.typography[variant].fontStyle ?? theme.typography.default.fontStyle,
-        fontWeight: theme.typography[variant].fontWeight ?? theme.typography.default.fontWeight,
-        fontVariant: theme.typography[variant].fontVariant ?? theme.typography.default.fontVariant
+        fontFamily: theme.typography[variant]?.fontFamily ?? theme.typography.default.fontFamily,
+        fontSize: theme.typography[variant]?.fontSize ?? theme.typography.default.fontSize,
+        fontStyle: theme.typography[variant]?.fontStyle ?? theme.typography.default.fontStyle,
+        fontWeight: theme.typography[variant]?.fontWeight ?? theme.typography.default.fontWeight,
+        fontVariant: theme.typography[variant]?.fontVariant ?? theme.typography.default.fontVariant
     }
 }))
